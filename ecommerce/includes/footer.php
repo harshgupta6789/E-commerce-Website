0c7235a38e1144b020d4cda3212a63993461cf18
harshgupta6789 <harshhvh999@gmail.com>
2019-11-15 23:53:44 +0530

    First commit

---
+++ ecommerce/includes/footer.php
@@ -0,0 +1,19 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <?php $r = rand(1,4) ?>
+        <?php if($r == 1) : ?>
+            <strong>“The odds of going to the store for a loaf of bread and coming out with only a loaf of bread are three billion to one.” -Erma Bombeck</strong>
+        <?php endif; ?>
+        <?php if($r == 2) : ?>
+            <strong>“When women are depressed, they eat or go shopping. Men invade another country. It’s a whole different way of thinking.” -Elayne Boosler</strong>
+        <?php endif; ?>
+        <?php if($r == 3) : ?>
+            <strong>“I haven’t reported my missing credit card to the police because whoever stole it is spending less than my wife.” -Ilie Nastase</strong>
+        <?php endif; ?>
+        <?php if($r == 4) : ?>
+            <strong>“The woman just ahead of you at the supermarket checkout has all the delectable groceries you didn’t even know they carried.” -Mignon McLaughlin</strong>
+        <?php endif; ?>
+      </div>
+    </div>
+</footer>
